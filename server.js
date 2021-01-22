2c54361a12f53f7d284484ce10009bf290ed096b
Riccardo Gai <r.gai@sintraconsulting.eu>
2021-01-22 21:21:53 +0100

    fix server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/doddle-classifier-app'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist//doddle-classifier-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
