8642a8847871189a59838c5e520490ed4897211f
Riccardo Gai <r.gai@sintraconsulting.eu>
2021-02-01 22:26:12 +0100

    working

--- src/app/core/core/models/doddle-classifier.model.ts
+++ src/app/core/core/models/doddle-classifier.model.ts
@@ -14,10 +14,23 @@ export class DoddleClassifier {
 
   async predict(data: tf.Tensor) {
     const argMax = await (this.model.predict(
-      data.reshape([1, 28, 28, 1])
+      data // .reshape([1, 28, 28, 1])
     ) as Tensor)
       .argMax(-1)
       .data();
     return this.classes[argMax[0]];
+  }
+
+  async predictTopN(data: tf.Tensor, n: number) {
+    const prediction = await (this.model.predict(
+      data // .reshape([1, 28, 28, 1])
+    ) as Tensor).data();
+    const topValues = Array.from(
+      [...prediction].sort((a, b) => b - a).slice(0, n)
+    );
+    return topValues.map((x) => ({
+      label: this.classes[prediction.indexOf(x)],
+      accuracy: x,
+    }));
   }
 }
